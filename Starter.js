4d3b971be6f184e99650f353225923c8f8b31890
Tony-the-best <250257675+Tony-the-best@users.noreply.github.com>
2026-01-04 21:06:45 -0500

    Create Starter.js

---
+++ Starter.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  merk zone aimbot test in PRIV server only
+// @author       You
+// @match        http:https://www.merc.zone/
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant better aim
+// ==/UserScript==
